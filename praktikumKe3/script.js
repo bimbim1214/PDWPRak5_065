186fdccbfe53d367c1c8f14f2b8d81a60e12266f
bimbim1214 <bimoadityapangestu@gmail.com>
2024-03-19 09:51:51 +0700

    my First Commit

---
+++ praktikumKe3/script.js
@@ -0,0 +1,15 @@
+function showAlert(){
+    alert(Date(), tampil(), show())
+    Date()
+
+    let tanggal = new Date();
+    Document.getElementById("hasil").innerHTML = tanggal;
+    
+    function tampil(){
+        alert("Mobil JDM")
+    }
+
+    function show(){
+        alert("Mobil jepang berkelas ")
+    }
+}
